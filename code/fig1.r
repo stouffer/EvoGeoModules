08da290d9ddbc71df4d8a1460e89626d819549e0
Timothee Poisot <t.poisot@gmail.com>
2014-09-16 16:31:42 +1200

    Code for figure 1, to run overnight

---
+++ code/fig1.r
@@ -0,0 +1,39 @@
+library(paco)
+library(stringr)
+library(igraph)
+library(betalink)
+
+source("commons.r")
+
+load("webs.Rdata")
+mw <- metaweb(raw)
+
+host_tree <- read.tree("../data/host.tre")
+host_tree$tip.label <- str_replace(host_tree$tip.label, "_", " ")
+para_tree <- compute.brlen(read.tree("../data/para.tre"))
+para_tree$tip.label <- str_replace(para_tree$tip.label, "_", " ")
+
+A <- incidence(mw)
+
+h_mat <- cophenetic(host_tree)[colnames(A), colnames(A)]
+p_mat <- cophenetic(para_tree)[rownames(A), rownames(A)]
+
+D <- proc_analysis(paco(pre_paco(H=h_mat, P=p_mat, HP=A)), nperm=10000)
+save(D, file="D.Rdata")
+
+local_paco <- function(n, hc, pc)
+{
+   blues <- V(n)$name[degree(n, mode="out")>0]
+   reds <- V(n)$name[degree(n, mode="in")>0]
+   reduced_A <- A[blues,reds]
+   local_A <- incidence(n)
+   D <- proc_analysis(paco(pre_paco(H=h_mat, P=p_mat, HP=A)))
+   regD <- proc_analysis(paco(pre_paco(H=h_mat[reds,reds], P=p_mat[blues,blues], reduced_A)), nperm=5000)
+   locD <- proc_analysis(paco(pre_paco(H=h_mat[reds,reds], P=p_mat[blues,blues], local_A)), nperm=5000)
+   output <- data.frame(loc=locD$gof$p, reg=regD$gof$p)
+   print(output)
+   return(output)
+}
+
+fig1dat <- ldply(raw, local_paco)
+save(fig1dat, "paco_fig1.Rdata")
