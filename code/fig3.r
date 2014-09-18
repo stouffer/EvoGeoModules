f9f1bcbb4fd51addaf11b716ef194cbba5444beb
Timothee Poisot <t.poisot@gmail.com>
2014-09-18 16:07:20 +1200

    Fixed fig3

--- code/fig3.r
+++ code/fig3.r
@@ -3,5 +3,5 @@ load("paco_fig3.Rdata")
 library(ggplot2)
 
 pdf(file="../figures/figure3.pdf")
-ggplot(fig3dat, aes(x=proba, y=stat, size=weight)) + geom_point() + geom_smooth() + theme_minimal()
+print(ggplot(fig3dat, aes(x=proba, y=stat, size=weight)) + geom_point() + geom_smooth() + theme_minimal())
 dev.off()
