507c010470ffb7860a41eabb15fbf817882e70da
Timothee Poisot <t.poisot@gmail.com>
2014-11-19 14:51:28 +1300

    Figure 3 updated

--- code/fig3.r
+++ code/fig3.r
@@ -2,6 +2,9 @@ load("paco_fig3.Rdata")
 
 library(ggplot2)
 
+
+fig3dat$vary = cut(fig3dat$proba, breaks=seq(0.0, 1.0, 0.2))
+
 pdf(file="../figures/figure3.pdf")
-print(ggplot(fig3dat, aes(x=proba, y=stat, size=weight)) + geom_point() + geom_smooth() + theme_minimal())
+print(ggplot(fig3dat, aes(x=vary, y=stat, fill=vary)) + geom_violin() + scale_y_log10() + geom_jitter() + theme_bw() + xlab("Interaction spatial consistency") + ylab("Importance for coevolution")) 
 dev.off()
