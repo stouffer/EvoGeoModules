94ad489f31b69fd23609d2c12975964acd64646d
Timothée Poisot <tim@poisotlab.io>
2014-12-15 08:06:34 +1300

    Figure 3

--- code/fig3.r
+++ code/fig3.r
@@ -4,7 +4,7 @@ fig3dat$vary = cut(fig3dat$proba, breaks=seq(0.0, 1.0, 0.2))
 
 pdf(file="../figures/figure3.pdf", height=6)
 
-boxplot(stat~vary, fig3dat, ylim=c(0,3e4), lty=1, pch=19, xlab="Interaction spatial consistency", ylab="Contribution to Coevolution")
-points(jitter(match(fig3dat$vary, levels(fig3dat$vary))), fig3dat$stat, pch=19, cex=fig3dat$weight/7, col="darkgrey")
+boxplot(stat~vary, fig3dat, ylim=c(0,3e4), lty=1, pch=19, xlab="Interaction spatial consistency", ylab="Interaction-level contribution to coevolutionary signal")
+points(jitter(match(fig3dat$vary, levels(fig3dat$vary))), fig3dat$stat, pch=21, cex=fig3dat$weight/7, bg=rgb(0, 0, 0, alpha=0.2), col=NA)
 
 dev.off()
